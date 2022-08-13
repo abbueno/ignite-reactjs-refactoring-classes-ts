12eb8a150552a7808c1168f74d38f41b880a84a1
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2022-08-13 16:17:13 -0300

    ok

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,43 @@
-import { Component } from 'react';
-import { FiPlusSquare } from 'react-icons/fi';
+import styled from 'styled-components';
 
-import { Container } from './styles';
-import Logo from '../../assets/logo.svg';
+export const Container = styled.div`
+  background: #c72828;
+  padding: 30px 0;
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+  header {
+    width: 1280px;
+    margin: 0 auto;
+    padding: 0 0 160px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button
-                type="button"
-                onClick={openModal}
-              >
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    )
+    nav {
+      div {
+        button {
+          font-weight: 600;
+          border-radius: 8px;
+          border: 0;
+          background: #39b100;
+          color: #fff;
+
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+
+          .text {
+            padding: 16px 24px;
+          }
+
+          .icon {
+            display: flex;
+            padding: 16px 16px;
+            background: #41c900;
+            border-radius: 0 8px 8px 0;
+            margin: 0 auto;
+          }
+        }
+      }
+    }
   }
-};
-
-export default Header;
+`;
